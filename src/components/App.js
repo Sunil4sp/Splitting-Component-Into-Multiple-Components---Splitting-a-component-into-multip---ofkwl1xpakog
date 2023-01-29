dc57fc734d9e3ecbfbe1786d6eac4028a2a7f010
Sunil Kumar Pal <110683501+Sunil4sp@users.noreply.github.com>
2023-01-29 19:16:33 +0530

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+import Main from "./Main";
+import Footer from "./Footer";
+function App() {
+    return (
+        <div className="container">
+            <header id="my-header">This is the header</header>
+            <main id="my-main">This is the main</main>
+            <footer id="my-footer">This is the footer</footer>
+        </div>
+    );
+}
+
+export default App;
